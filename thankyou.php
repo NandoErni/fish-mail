730413fdef252b6c6c6decc1dd222060014909fe
Pascal <pascal.leone@stud.kftg.ch>
2019-01-11 16:29:22 +0100

    fish it to the defer

--- thankyou.php
+++ thankyou.php
@@ -63,7 +63,7 @@ include "config.php";
 
 ?>
 
-<script src="js/script.js"></script>
+<script defer src="js/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
