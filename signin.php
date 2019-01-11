5929fdad8ecf7b207941c357f0637e5bcd7f5c85
nando <nando.erni@outlook.de>
2019-01-11 16:20:44 +0100

    Fish it to the limit ah-aha

--- signin.php
+++ signin.php
@@ -32,13 +32,16 @@ include "config.php";
     <?php
     if(isset($_POST['loginSubmit'])){
         if(login($_POST['inputusername'], $_POST['inputpassword'])){
-
+            header("Location: index.php"); /* Redirect browser */
+            $_SESSION['userName'] = $_POST['inputusername'];
+        } else {
+            echo "<h6 class=\"\">Invalid login</h6>";
         }
     }
 
     ?>
 
-    <h6 class="">Invalid login</h6>
+
     <label for="inputusername" class="sr-only">Username</label>
     <input type="text" id="inputusername" name="inputusername" class="form-control" placeholder="Username"
            required autofocus>
@@ -51,16 +54,5 @@ include "config.php";
     <button class="btn btn-lg btn-primary btn-block" type="submit" name="loginSubmit">Sign in</button>
     <p class="mt-5 mb-3 text-muted"><?php echo copyright() ?></p>
 </form>
-
-<script src="js/validate.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-        crossorigin="anonymous"></script>
 </body>
 </html>
