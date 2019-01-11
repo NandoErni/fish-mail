f41b7a4f52ca66f79e7316f8dcf20449138687ec
nando <nando.erni@outlook.de>
2019-01-11 15:40:01 +0100

    Fish it to the limit ah-aha

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ include "config.php";
                 <a class="nav-link" href="#">Pond <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Settings</a>
+                <a class="nav-link" href="settings.php">Settings</a>
             </li>
         </ul>
     </div>
@@ -80,6 +80,7 @@ include "config.php";
     </div>
 </main>
 
+<script src="js/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
